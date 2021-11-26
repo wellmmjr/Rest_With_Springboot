d49a00362721121ad76313f8348eb681fa729958
Wellington Jr <wellingtonmm.jr@gmail.com>
2021-11-26 19:04:21 -0300

    Auth
    authentication with jwt and springsecurity

--- RestWithSpringboot/src/main/java/br/com/wellmmjr/exception/ResourceNotFoundException.java
+++ RestWithSpringboot/src/main/java/br/com/wellmmjr/exception/ResourceNotFoundException.java
@@ -1,10 +1,11 @@
 package br.com.wellmmjr.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ResourceNotFoundException extends AuthenticationException {
 
 	
 	private static final long serialVersionUID = 1L;
--- RestWithSpringboot/src/main/java/br/com/wellmmjr/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ RestWithSpringboot/src/main/java/br/com/wellmmjr/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.wellmmjr.exception.ExceptionResponse;
+import br.com.wellmmjr.exception.InvalidJWTAuthenticationException;
 import br.com.wellmmjr.exception.ResourceNotFoundException;
 
 @RestController
@@ -30,6 +31,16 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+		ExceptionResponse exceptionRresponse = 
+				new ExceptionResponse(
+						new Date(), 
+						ex.getMessage(), 
+						request.getDescription(false));
+		return new ResponseEntity<>(exceptionRresponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJWTAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> invalidJWTAuthenticationException(Exception ex, WebRequest request){
 		ExceptionResponse exceptionRresponse = 
 				new ExceptionResponse(
 						new Date(), 
